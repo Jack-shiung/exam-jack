2ec9b69cbf00904da8f4846a7c876a178bce4e7f
jack shiung <shiungneplus@gmail.com>
2026-01-10 11:10:43 +0800

    add solution logic for finish is single cycle check

--- src/utils/solution.ts
+++ src/utils/solution.ts
@@ -11,9 +11,9 @@ export const solution = (a: Array<number>, b: Array<number>): boolean => {
    * @param key start point a[key]
    * @param value end point b[key]
    */
-  const stepMap = new Map()
-  const aPointSet = new Set()
-  const bPointSet = new Set()
+  const stepMap = new Map<number, number>()
+  const aPointSet = new Set<number>()
+  const bPointSet = new Set<number>()
 
   for (let i = 0; i < a.length; i++) {
     /** 重複enter point */
@@ -29,18 +29,26 @@ export const solution = (a: Array<number>, b: Array<number>): boolean => {
    * @param firstPoint 起始步
    * @param endPoint 終點步
    * @param curPosition 當前位置
+   * @param curStepIdx 執行while loop 的計步器[index]
+   * @param visited 使用Set object 來記錄每段步伐 如果發生重複表示不是一個循環圓
    */
   const firstPoint = a[0]
-  let curPositon = firstPoint
-  for (let i = 0; i < nStep; i++) {
-    // 使用起點步伐去看2點一線的終點在哪
-    const endPoint = stepMap.get(curPositon)
-    // 找無對應的終點 步伐終止
+  const visited = new Set<number>()
+  let curPosition = firstPoint
+  let curStepIdx = 0
+  while (curStepIdx < nStep) {
+    // 1. 重複的紀錄點
+    if (visited.has(curPosition)) return false
+    visited.add(curPosition)
+    // 2. 使用起點步伐去看2點一線的終點在哪
+    const endPoint = stepMap.get(curPosition)
+    // 3. 找無對應的終點 步伐終止
     if (endPoint === undefined) return false
-    // 當前步數紀錄點為此次loop 的終點, 下個循環繼續找尋
-    curPositon = endPoint
+    // 4. 當前步數紀錄點為此次loop 的終點, 下個循環繼續找尋
+    curPosition = endPoint
+    curStepIdx++
   }
 
   // 確認所有loop 完畢後 最後停留的位置在起始點 就為一個cycle
-  return firstPoint === curPositon
+  return firstPoint === curPosition
 }
